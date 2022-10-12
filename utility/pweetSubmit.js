4cb06fcb7515c8990c406e90c8933547bd78a6f5
kichul44 <kichul7493@gmail.com>
2022-10-12 16:29:17 +0900

    creatorName default value set

--- utility/pweetSubmit.js
+++ utility/pweetSubmit.js
@@ -4,10 +4,11 @@ import uploadImage from "./uploadImage";
 
 const pweetSubmit = async (image, pweet, user) => {
   const [imageName, imageUrl] = await uploadImage(image, user.uid);
+  const userName = user.name ? user.name : "anonymous";
   const pweetObj = {
     text: pweet,
     createAt: new Date(),
-    creatorName: user.name,
+    creatorName: userName,
     creatorId: user.uid,
     creatorImage: user.photoURL,
     imageUrl,
